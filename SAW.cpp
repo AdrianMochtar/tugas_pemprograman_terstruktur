7f596e0d4be8e7ddfe3690d4b6d18b76a848dd09
AdrianMochtar <mochtaradrian.am@gmail.com>
2018-01-03 07:56:44 +0800

    membuat matriks x dengan do while

--- SAW.cpp
+++ SAW.cpp
@@ -3,11 +3,35 @@
 using namespace std;
 
 int main(){
-	// DEKLARASI VARIABEL
-	double a1c1,a1c2,a1c3,a1c4,a1c5;
-	double a2c1,a2c2,a2c3,a2c4,a2c5;
-	double a3c1,a3c2,a3c3,a3c4,a3c5;
 	
+	// SETTING PRESISI DESIMAL
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(4);	
+
+	//DEKLARASI VARIABEL DAN ISI DATA
+	double a1c[5]={4,4,5,3,3};
+	double a2c[5]={3,3,4,2,3};
+	double a3c[5]={5,4,2,2,2};
+	
+	//MEMBUAT MATRIKS X DENGAN DO WHILE
+	cout << "MATRIKS X : "<< endl;
+	
+	int x=0;
+		do{
+		cout << a1c[x]<< " | ";	x++;}
+		while (x<5);cout << endl;
+	int y=0;	
+		do{
+		cout << a1c[y]<< " | ";	y++;}
+		while (y<5);cout << endl;
+	int z=0;	
+		do{
+		cout << a3c[z]<< " | ";	z++;}
+		while (z<5);cout << endl;
+		
+	cout << endl;
+	// DEKLARASI VARIABEL
 	double Ra1c1,Ra1c2,Ra1c3,Ra1c4,Ra1c5;
 	double Ra2c1,Ra2c2,Ra2c3,Ra2c4,Ra2c5;
 	double Ra3c1,Ra3c2,Ra3c3,Ra3c4,Ra3c5;
@@ -19,11 +43,6 @@ int main(){
 	double w1,w2,w3,w4,w5;
 	double mc1,mc2,mc3,mc4,mc5;
 	double ha1,ha2,ha3;
-	
-	// SETTING PRESISI DESIMAL
-	cout.setf(ios::fixed);
-	cout.setf(ios::showpoint);
-	cout.precision(4);
 
 	// ISI DATA
 	w1=5;
@@ -31,24 +50,6 @@ int main(){
 	w3=4;
 	w4=4;
 	w5=2;
-	
-	a1c1=4;
-	a1c2=4;
-	a1c3=5;
-	a1c4=3;
-	a1c5=3;
-	
-	a2c1=3;
-	a2c2=3;
-	a2c3=4;
-	a2c4=2;
-	a2c5=3;
-	
-	a3c1=5;
-	a3c2=4;
-	a3c3=2;
-	a3c4=2;
-	a3c5=2;
 	
 	// AMBIL NILAI MAX TIAP KRITERIA
 	mc1=max(a1c1,max(a2c1,a3c1));
@@ -101,11 +102,6 @@ int main(){
 	ha3=RWa3c1+RWa3c2+RWa3c3+RWa3c4+RWa3c5;
 		
 	// MENAMPILKAN NILAI
-	cout << "MATRIKS X :"<<endl;
-	cout << a1c1 <<" | "<<a1c2<<" | "<<a1c3<<" | "<<a1c4<<" | "<<a1c5<<endl;
-	cout << a2c1 <<" | "<<a2c2<<" | "<<a2c3<<" | "<<a2c4<<" | "<<a2c5<<endl;
-	cout << a3c1 <<" | "<<a3c2<<" | "<<a3c3<<" | "<<a3c4<<" | "<<a3c5<<endl;
-	cout << endl;
 	cout << "MATRIKS R :"<<endl;
 	cout << Ra1c1 <<" | "<<Ra1c2<<" | "<<Ra1c3<<" | "<<Ra1c4<<" | "<<Ra1c5<<endl;
 	cout << Ra2c1 <<" | "<<Ra2c2<<" | "<<Ra2c3<<" | "<<Ra2c4<<" | "<<Ra2c5<<endl;
